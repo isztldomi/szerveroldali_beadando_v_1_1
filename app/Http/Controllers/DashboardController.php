622ec3f4782ac328d492920c42ddb10ffd30b720
isztldomi <isztl.domi@gmail.com>
2025-11-02 21:01:03 +0100

    style fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Ticket;
 
 class DashboardController extends Controller
 {
@@ -15,7 +18,47 @@ class DashboardController extends Controller
             // Ha nem admin, vissza a főoldalra
             return redirect('/');
         }
+        // Összes esemény
+        $totalEvents = Event::count();
 
-        return view('dashboard');
+        // Összes eladott jegy
+        $totalTickets = Ticket::count();
+
+        // Összes bevétel
+        $totalIncome = Ticket::sum('price');
+
+        // 3 legnépszerűbb ülőhely (seat_number) és eladott jegyek száma
+        $topSeats = Ticket::select('seat_number', DB::raw('COUNT(*) as sold_count'))
+            ->groupBy('seat_number')
+            ->orderByDesc('sold_count')
+            ->limit(3)
+            ->get();
+
+
+        // Legfrissebb események, 5 per oldal
+        $events = Event::withCount(['tickets as sold_tickets_count' => function ($query) {
+            $query->where('sold', true); // vagy ahol eladott a logika
+        }])
+        ->withCount('tickets as total_tickets_count')
+        ->paginate(5);
+
+        // Eseményekhez számoljuk a bevételt
+        foreach ($events as $event) {
+            // eladott jegyek bevétele
+            $event->revenue = Ticket::where('event_id', $event->id)
+                                    ->where('sold', true)
+                                    ->sum('price');
+
+            // szabad jegyek
+            $event->available_tickets = $event->total_tickets_count - $event->sold_tickets_count;
+        }
+
+        return view('dashboard', [
+            'totalEvents' => $totalEvents,
+            'totalTickets' => $totalTickets,
+            'totalIncome' =>$totalIncome,
+            'topSeats' => $topSeats,
+            'events '=> $events
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Irányítópult') }}
         </h2>
     </x-slot>
 
@@ -12,6 +12,12 @@
                     {{ __("You're logged in!") }}
                 </div>
             </div>
+
+
+
+
+
+
         </div>
     </div>
 </x-app-layout>
