3c99266079fe73fec996231cb97e00ecb925626d
isztldomi <isztl.domi@gmail.com>
2025-11-02 20:41:01 +0100

    DashboardController only admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,14 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        //
+        // itt tartok
+        $user = auth()->user();
+
+        if (!$user->isAdmin()) {
+            // Ha nem admin, vissza a főoldalra
+            return redirect('/');
+        }
+
+        return view('dashboard');
     }
 }
